5afa4e41c9eb039263dd7abd7f4798dcaea9b49d
Mohammed Kamil <mohammedkamilhussain2911@gmail.com>
2025-02-04 11:25:26 +0530

    my first commit

---
+++ day2/IfElse.java
@@ -0,0 +1,11 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int a=10,b=20;
+        if(a>b){
+            System.err.println(a+ "is greater");
+        }
+        else{
+            System.err.println(b+ "is greater");
+        }
+    }
+}
