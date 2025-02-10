4c3b6fc6a0272b171d5c6f0c354ebe10c8a93c19
Mohammed Kamil <mohammedkamilhussain2911@gmail.com>
2025-02-10 12:04:39 +0530

    day 7 morning 2nd half completed

---
+++ day7/StringBuilderDemo2.java
@@ -0,0 +1,13 @@
+public class StringBuilderDemo2 {
+    public static void main(String[] args) {
+        String s="hello";
+        StringBuilder sb=new StringBuilder("abc");
+        sb.append("world");
+        s=sb.toString();
+        System.out.println(s);
+        StringBuilder sb1=new StringBuilder("cat");
+        sb1.reverse();
+        System.out.println(sb1);
+    }
+}
+
